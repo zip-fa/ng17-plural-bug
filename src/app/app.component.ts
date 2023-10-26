2b22069fdb975fef0b82787a1bcfa1c655fa184b
zip-fa <61551308+zip-fa@users.noreply.github.com>
2023-10-26 20:06:50 +0300

    bug repro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { I18nPluralPipe, registerLocaleData } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
+// import localeUk from '@angular/common/locales/uk';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [I18nPluralPipe, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng17-plural-bug';
+  messages: any[] = ['Message 1'];
+  messageMapping = {
+    'zero': $localize`:@@plurals.days_0:дней`,
+    'one': $localize`:@@plurals.days_1:день`,
+    'few': $localize`:@@plurals.days_2:дня`,
+    'many': $localize`:@@plurals.days_0:дней`,
+    'other': $localize`:@@plurals.days_0:дней`
+  };
+
+  constructor() {
+    // registerLocaleData(localeUk);
+  }
 }
